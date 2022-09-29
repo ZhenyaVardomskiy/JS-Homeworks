00a2e861cb935ca9697184291687b1fda1ae7a82
ZhenyaVardomskiy <zhenya.vardomskiy99@gmial.com>
2022-09-29 13:36:11 +0300

    1,2,3,4,5,6

---
+++ lesson-3/index.js
@@ -0,0 +1,50 @@
+// 1
+
+// for (let i = 1; i <= 50; i++ ) {
+//     console.log (i)
+// }
+
+// for (let j = 35; j >= 8; j-- ) {
+//     console.log (j)
+// }
+
+// 2
+
+// let g = 89;
+// for (; g >= 11; ){
+//     g--
+//      console.log(g + '<br>') 
+//  }
+
+// 3
+
+// let sum = 0;
+// for (let i = 1; i <= 100; i++ ) {
+//     console.log (sum +=i)
+// }
+
+// 4
+
+// let a=5;
+// let sum = 0;
+// for (let i = 1; i <= a; i++ ) {
+//     console.log (i)
+//     sum +=i
+// }
+// console.log(sum)
+
+// 5
+
+// for (let i = 8; i <= 56; i=i+2 ) {
+//     console.log (i)
+// }
+
+// 6
+
+for(let i = 2; i<=10; i++){
+    for(let a = 2; a<=10; a++){
+        document.write(i + "*" + a + "=" +(i*a) + "<br>");
+        
+    }
+    document.write('<br>');
+}
