e76c437b0d2ba64c56bdc96544e73161122f9f1a
ZhenyaVardomskiy <zhenya.vardomskiy99@gmial.com>
2022-10-02 18:22:07 +0300

    pochti sdelal

---
+++ lesson-4/index.js
@@ -0,0 +1,105 @@
+// 1
+
+// let arr1 = [1, 2, 3, 4, 5];
+// for (let i=0 ; i < arr1.length; i++){
+//     document.write( arr1[i]);
+// }
+
+
+// 2
+
+// let arr2 = [-2, -1, -3, 15, 0, -4, 2, -5, 9, -15, 0, 4, 5, -6, 10, 7];
+// for (let i = 0; i > -10; i++) {
+//     if (arr2[i]>-10 && arr2[i]<-3) {
+//       console.log(arr2[i]);
+//     }
+//   }
+
+
+// 3
+
+// let arr3 =[];
+
+// for (let i = 23; i<= 57; i++) {
+//     arr3.push(i);
+    
+// }
+
+// console.log(arr3);
+
+// function arraySum(array){
+//     let sum = 0;
+//     for(let i = 0; i < array.length; i++){
+//         sum += array[i];
+//         }
+//     console.log(sum);
+//     }
+//     arraySum(arr3);
+
+
+// 4
+
+// let arr4 = ['10', '20', '30', '50', '235', '3000'];
+
+// arr4.forEach((item) => {
+//   if (item.startsWith('1') || item.startsWith('2') || item.startsWith('5')) {
+//     console.log(item);
+//   }
+// });
+
+
+// 5
+
+// let arr5 = ["ПН","ВТ","СР","ЧТ","ПТ","СБ","ВС"];
+
+// for (let i = 0; i< arr5.length; i++) {
+//     if (i > 4) {
+//         document.write('<b>' + arr5[i] + '</b>' + ' ');
+//     } else {
+//         document.write(arr5[i] + ' ');
+//     }
+// }
+
+
+// 6
+
+// let arr6 = ["кружка","вилка","чайник","стол","пес","холодос"];
+// console.log(arr6);
+
+// arr6[arr6.length] = "мышь";
+// console.log(arr6);
+
+
+// 7
+
+
+
+
+// 8
+
+// let arr8 =  [12, false, 'Текст', 4, 2, -5, 0];
+// arr8.reverse();
+// console.log(arr8);
+
+// 9
+
+
+
+
+
+// 10
+
+// let i = 0,
+//     j = 0;
+// let max = 6;
+// let space = "",
+//     star = "";
+
+// while (i < max) {
+//   space = "";
+//   star = "";
+//   for (j = 0; j < max - i; j++) space += " ";
+//   for (j = 0; j < 2 * i + 1; j++) star += "^";
+//   console.log(space + star);
+//   i++;
+// }
---
+++ lesson-5/index.js
@@ -0,0 +1,50 @@
+// 1
+
+// function first(a,b,c) {
+//     console.log((a+b)/c);
+// }
+
+// first(15,15,3);
+
+
+// 2
+
+// function second(a,b) {
+//     console.log(a**2 , b**3);
+// }
+
+// second(2,3);
+
+
+// 3
+
+// function min(a,b) {
+//     if (a < b) {
+//       console.log (a);
+//     } else {
+//         console.log (b);
+//     }
+//   }
+
+// min(50,16); 
+// min(89,62); 
+// min(-61,105);
+
+// 4
+
+
+
+// 5
+
+// function isEven(a) {
+//     if (a % 2 == 0) {
+//       console.log (true);
+//     } else {
+//         console.log (false);
+//     }
+//   }
+
+// isEven(77)   
+
+// 6
+
